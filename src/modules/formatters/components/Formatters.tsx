efff163b25d6642aa2bfacf6001adb20067388cd
Nikita Bereziuk <nikita.bereziuk@sysgears.com>
2022-10-10 22:40:07 +0300

    Add opportunity to delete formatters

--- src/modules/formatters/components/Formatters.tsx
+++ src/modules/formatters/components/Formatters.tsx
@@ -1,13 +1,30 @@
-import { FC } from "react";
-import { Table } from "react-bootstrap";
-import { useDexie } from "../../../module-core/database";
+import { FC, useState } from "react";
+import { Button, Spinner, Table } from "react-bootstrap";
+import { useDexie, useLazyDexie } from "../../../module-core/database";
 import { useTranslate } from "../../../module-core/i18n-js";
+import { delay, useLoading } from "../../common";
 import { useDocumentTypeNameById } from "../../document-types";
 
 const Formatters: FC = () => {
     const i18n = useTranslate()
     const typeNames = useDocumentTypeNameById()
     const formatters = useDexie(db => db.formatters.toArray())
+
+    const [removing, setRemoving] = useState(false)
+    const [removingId, setRemovingId] = useState<number>()
+
+    const [removeFormatter] = useLazyDexie<unknown, [number | undefined]>(async (db, id) => {
+        setRemovingId(id)
+        try {
+        await delay(1000)
+        await db.formatters.where('id').equals(id as number).delete()
+        } catch (err) {
+        throw err
+        } finally {
+        setRemovingId(undefined)
+        }
+    })
+    const removeHandler = useLoading(removeFormatter, setRemoving)
 
     if (!typeNames || !formatters) {
         return <h1>{i18n.t('common.loading')}</h1>
@@ -17,6 +34,8 @@ const Formatters: FC = () => {
         return <h1>{i18n.t('common.nothing')}</h1>
     }
 
+    const getIsIAmRemoving = (id: number | undefined) => removing && removingId === id
+
     return (<>
         <Table>
             <thead>
@@ -24,6 +43,7 @@ const Formatters: FC = () => {
                     <th>Id</th>
                     <th>Name</th>
                     <th>Document Type</th>
+                    <th>Actions</th>
                 </tr>
             </thead>
             <tbody>
@@ -32,6 +52,18 @@ const Formatters: FC = () => {
                         <td>{formatter.id}</td>
                         <td>{formatter.name}</td>
                         <td>{typeNames.get(formatter.type) || 'Missed type'}</td>
+                        <td width={200}>
+                            <Button
+                                variant="danger"
+                                disabled={getIsIAmRemoving(formatter.id)}
+                                onClick={() => removeHandler(formatter.id)}
+                                size="sm"
+                            >
+                                { getIsIAmRemoving(formatter.id) && <Spinner animation="grow" size="sm" /> }
+                                { getIsIAmRemoving(formatter.id) && i18n.t('documents.table.removingAction') }
+                                { !getIsIAmRemoving(formatter.id) && i18n.t('documents.table.removeAction') }
+                            </Button>
+                        </td>
                     </tr>
                 )} 
             </tbody>
